73c50b41d239ff61bb3572e5053cf0c03beb74b5
juan lobos <juan.lobosh@grupomok.com>
2019-12-04 20:23:16 -0300

    first commit

---
+++ WebApplication6/Controllers/HomeController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication6.Models;
+
+namespace WebApplication6.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> About(string nombre)
+        {
+            var users = new Usuarios();
+            var usuario = users.listaUsers().Where(z => z.Nombre == nombre).FirstOrDefault();
+
+            var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, usuario.Nombre),
+                    new Claim("FullName", usuario.Nombre),
+                    new Claim(ClaimTypes.Role, usuario.Rol),
+                };
+            var claimsIdentity = new ClaimsIdentity(
+                claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var authProperties = new AuthenticationProperties
+            {
+                //AllowRefresh = <bool>,
+                // Refreshing the authentication session should be allowed.
+
+                //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+                // The time at which the authentication ticket expires. A 
+                // value set here overrides the ExpireTimeSpan option of 
+                // CookieAuthenticationOptions set with AddCookie.
+
+                //IsPersistent = true,
+                // Whether the authentication session is persisted across 
+                // multiple requests. When used with cookies, controls
+                // whether the cookie's lifetime is absolute (matching the
+                // lifetime of the authentication ticket) or session-based.
+
+                //IssuedUtc = <DateTimeOffset>,
+                // The time at which the authentication ticket was issued.
+
+                //RedirectUri = <string>
+                // The full path or absolute URI to be used as an http 
+                // redirect response value.
+            };
+
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                authProperties);
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+
+            #region snippet1
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            #endregion
+
+            return RedirectToAction("contact","home");
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+        [Authorize(Roles = "Root")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication6/Usuarios.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication6
+{
+    public class Usuarios
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Rol { get; set; }
+
+        public List<Usuarios> listaUsers()
+        {
+            var lista = new List<Usuarios>();
+            lista.Add(new Usuarios { Id = 1, Nombre = "jhon", Rol = "Root" });
+            lista.Add(new Usuarios { Id = 2, Nombre = "marymar", Rol = "Persona" });
+            lista.Add(new Usuarios { Id = 3, Nombre = "paula", Rol = "Persona" });
+            return lista;
+        }
+    }
+}
